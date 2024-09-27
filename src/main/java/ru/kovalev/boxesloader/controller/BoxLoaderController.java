f7c091e8173ecded2c8319282f383f89cd001593
Nikolay Kovalev <Nikolay.Kovalev.SPB@mail.ru>
2024-09-27 16:27:11 +0300

    Добавляю логирование

--- src/main/java/ru/kovalev/boxesloader/controller/BoxLoaderController.java
+++ src/main/java/ru/kovalev/boxesloader/controller/BoxLoaderController.java
@@ -1,10 +1,12 @@
 package ru.kovalev.boxesloader.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import ru.kovalev.boxesloader.interfaces.Action;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class BoxLoaderController {
     private final Map<Integer, Action> actions;
 
@@ -14,10 +16,16 @@ public class BoxLoaderController {
 
     public void addAction(int option, Action action) {
         actions.put(option, action);
+        log.info("Добавлено действие для опции: {}", option);
     }
 
     public void executeAction(int option) {
-        Action action = actions.getOrDefault(option, () -> System.out.println("Некорректный ввод. " + option));
+        log.info("Попытка выполнить действие для опции: {}", option);
+        Action action = actions.getOrDefault(option, () -> {
+            log.warn("Некорректный ввод: {}.", option);
+            throw new IllegalArgumentException("Некорректный ввод. " + option);
+        });
         action.execute();
+        log.info("Выполнено действие для опции: {}", option);
     }
 }
